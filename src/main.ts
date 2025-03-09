90bb7d44cb317f8bcc9c09d020cff6dc748d9daa
Dennis Ranke <dennis.ranke@gmail.com>
2025-03-09 21:08:34 +0100

    fetch black moves from opening explorer

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { Chessground } from 'chessground';
-import { Key, MoveMetadata } from 'chessground/types';
-import { Chess, SQUARES } from 'chess.js';
+import * as cg from 'chessground/types';
+import { Chess, SQUARES, Square } from 'chess.js';
 
 const config = {
   movable: {
-    free: false
+    free: false,
+    colors: 'white'
   }
 };
-const cg = Chessground(document.getElementById('board')!, config);
+const board = Chessground(document.getElementById('board')!, config);
 
 const chess = new Chess();
 
@@ -19,7 +20,7 @@ function setPlayerMove() {
       dests.set(square, moves.map(m => m.to));
     }
   }
-  cg.set({
+  board.set({
     fen: chess.fen(),
     movable: {
       dests,
@@ -30,11 +31,65 @@ function setPlayerMove() {
   });
 }
 
-function applyPlayerMove(from: Key, to: Key, _meta: MoveMetadata) {
-  chess.move({ from, to });
-  if(!chess.isGameOver()) {
-    setPlayerMove();
+function applyPlayerMove(from: cg.Key, to: cg.Key, _meta: cg.MoveMetadata) {
+  const move = { from, to, promotion: undefined as string | undefined };
+  const piece = chess.get(from as Square);
+  if(piece && piece.type == 'p' && (to.charAt(1) == '8' || to.charAt(1) == '1')) {
+    move.promotion = 'q';
   }
+  chess.move(move);
+  if(!chess.isGameOver()) {
+    doDbMove();
+  }
+}
+
+async function doDbMove() {
+  board.set({
+    fen: chess.fen()
+  });
+
+  const response = await fetch('https://explorer.lichess.ovh/lichess' + toQuery({
+    fen: chess.fen(),
+    speeds: 'blitz',
+    ratings: '1800',
+    topGames: 0,
+    recentGames: 0
+  }));
+
+  const entry = await response.json();
+
+  let total = 0;
+  let moves: {move: string, count: number}[] = [];
+  for(let move of entry.moves) {
+    const count = move.white + move.draws + move.black;
+    total += count;
+    moves.push({move: move.san, count});
+  }
+
+  let cursor = Math.random() * total;
+  for(let i = 0; i < moves.length; ++i) {
+    const move = moves[i];
+    cursor -= move.count;
+    if(cursor <= 0) {
+      chess.move(move.move);
+      if(!chess.isGameOver()) {
+        setPlayerMove();
+        break;
+      }
+    }
+  }
+}
+
+function toQuery(query: Object): string {
+  let q = '';
+  for(let key in query) {
+    let value = query[key];
+    q += (q.length == 0 ? '?' : '&');
+    q += key;
+    q += '=';
+    q += encodeURIComponent(value);
+  }
+  return q;
 }
 
 setPlayerMove();
