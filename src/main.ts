3390879b910d6669dc69dfd7eca1bf75c5b330c0
Dennis Ranke <dennis.ranke@gmail.com>
2025-03-10 22:45:18 +0100

    port to preact to prepare for ui

--- src/main.ts
+++ src/main.ts
@@ -1,84 +1,113 @@
 import { Chessground } from 'chessground';
+import { Api } from 'chessground/api';
 import * as cg from 'chessground/types';
 import { Chess, SQUARES, Square } from 'chess.js';
+import { render, h } from 'preact';
+import { useRef, useEffect, useState } from 'preact/hooks';
 
-const config = {
-  movable: {
-    free: false,
-    colors: 'white'
-  }
+const Board = ({moves, color, onMove}) => {
+  const boardDiv = useRef(null as HTMLElement | null);
+  const boardRef = useRef(null as Api | null);
+
+  useEffect(() => {
+    if(!boardRef.current) {
+      boardRef.current = Chessground(boardDiv.current!, { movable: { free: false } });
+    }
+
+    const board = boardRef.current;
+
+    const chess = new Chess();
+    for(const move of moves) {
+      chess.move(move);
+    }
+    
+    const dests = new Map();
+
+    if(chess.turn() == color) {
+      for(const square of SQUARES) {
+        const moves = chess.moves({ square, verbose: true });
+        if(moves.length > 0) {
+          dests.set(square, moves.map(m => m.to));
+        }
+      }
+    }
+    
+    board.set({
+      fen: chess.fen(),
+      turnColor: {w: 'white', b: 'black'}[chess.turn()] as cg.Color,
+      movable: {
+        color: {w: 'white', b: 'black'}[color],
+        dests,
+        events: {
+          after: (from, to) => {
+            const move = { from, to, promotion: undefined as string | undefined };
+            const piece = chess.get(from as Square);
+            if(piece && piece.type == 'p' && (to.charAt(1) == '8' || to.charAt(1) == '1')) {
+              move.promotion = 'q';
+            }
+            onMove(chess.move(move).san);
+          }
+        }
+      }
+    });
+  }, [moves, color]);
+
+  return h('div', {id: 'board', ref: boardDiv});
 };
-const board = Chessground(document.getElementById('board')!, config);
 
-const chess = new Chess();
+const App = () => {
+  const [moves, setMoves] = useState<string[]>([]);
+  const [color, setColor] = useState('w');
 
-function setPlayerMove() {
-  const dests = new Map();
-  for(const square of SQUARES) {
-    const moves = chess.moves({ square, verbose: true });
-    if(moves.length > 0) {
-      dests.set(square, moves.map(m => m.to));
-    }
-  }
-  board.set({
-    fen: chess.fen(),
-    movable: {
-      dests,
-      events: {
-        after: applyPlayerMove
+  useEffect(() => {
+    const whiteTurn = (moves.length & 1) == 0;
+    if(whiteTurn != (color == 'w')) {
+      const chess = new Chess();
+      for(const move of moves) {
+        chess.move(move);
       }
+
+      (async () => {
+        const response = await fetch('https://explorer.lichess.ovh/lichess' + toQuery({
+          fen: chess.fen(),
+          speeds: 'blitz',
+          ratings: '1800',
+          topGames: 0,
+          recentGames: 0
+        }));
+
+        const entry = await response.json();
+
+        let total = 0;
+        let dbMoves: {move: string, count: number}[] = [];
+        for(let move of entry.moves) {
+          const count = move.white + move.draws + move.black;
+          total += count;
+          dbMoves.push({move: move.san, count});
+        }
+
+        let cursor = Math.random() * total;
+        for(const move of dbMoves) {
+          cursor -= move.count;
+          if(cursor <= 0) {
+            const mvs = moves.slice();
+            mvs.push(move.move);
+            setMoves(mvs);
+            break;
+          }
+        }
+      })()
     }
-  });
-}
+  }, [moves, color])
 
-function applyPlayerMove(from: cg.Key, to: cg.Key, _meta: cg.MoveMetadata) {
-  const move = { from, to, promotion: undefined as string | undefined };
-  const piece = chess.get(from as Square);
-  if(piece && piece.type == 'p' && (to.charAt(1) == '8' || to.charAt(1) == '1')) {
-    move.promotion = 'q';
-  }
-  chess.move(move);
-  if(!chess.isGameOver()) {
-    doDbMove();
-  }
-}
+  return h(Board, {moves, color, onMove: (m: string) => {
+    const mvs = moves.slice();
+    mvs.push(m);
+    setMoves(mvs);
+  }});
+};
 
-async function doDbMove() {
-  board.set({
-    fen: chess.fen()
-  });
-
-  const response = await fetch('https://explorer.lichess.ovh/lichess' + toQuery({
-    fen: chess.fen(),
-    speeds: 'blitz',
-    ratings: '1800',
-    topGames: 0,
-    recentGames: 0
-  }));
-
-  const entry = await response.json();
-
-  let total = 0;
-  let moves: {move: string, count: number}[] = [];
-  for(let move of entry.moves) {
-    const count = move.white + move.draws + move.black;
-    total += count;
-    moves.push({move: move.san, count});
-  }
-
-  let cursor = Math.random() * total;
-  for(let i = 0; i < moves.length; ++i) {
-    const move = moves[i];
-    cursor -= move.count;
-    if(cursor <= 0) {
-      chess.move(move.move);
-      if(!chess.isGameOver()) {
-        setPlayerMove();
-        break;
-      }
-    }
-  }
-}
+render(h(App, null), document.body);
 
 function toQuery(query: Object): string {
   let q = '';
@@ -91,5 +120,3 @@ function toQuery(query: Object): string {
   }
   return q;
 }
-
-setPlayerMove();
